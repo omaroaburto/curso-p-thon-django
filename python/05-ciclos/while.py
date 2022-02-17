a2d099369d0e795ce87abcfda30efe33d8ab6ecf
Omaro Alexis Aburto Leiva <oaburto@alumnos.ubiobio.cl>
2022-02-17 03:50:38 -0300

    actualización de curso de python

--- python/05-ciclos/while.py
+++ python/05-ciclos/while.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 animales = []
 cantidad = int(input("Ingrese la cantidad de animales: "))
 while cantidad!=0:
     animal = input("Ingrese animal: ").strip().capitalize()
     animales.append(animal)
     cantidad-=1
-
-=======
-animales = []
-cantidad = int(input("Ingrese la cantidad de animales: "))
-while cantidad!=0:
-    animal = input("Ingrese animal: ").strip().capitalize()
-    animales.append(animal)
-    cantidad-=1
-
->>>>>>> 8921a0d69ee2bdb1f3aa6232c04828baab1a97e1
+ 
 print("lista de animales:\n",animales)
